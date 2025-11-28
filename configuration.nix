a8ffd08ac11b42acd1baa46ebd4244801e462985
dark1zinn <edilsonjuininho154@gmail.com>
2025-11-28 10:39:34 -0300

    chore: :fire: remove printer support (who needs that?)

--- configuration.nix
+++ configuration.nix
@@ -80,9 +80,6 @@
   # Configure console keymap
   console.keyMap = "br-abnt2";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
