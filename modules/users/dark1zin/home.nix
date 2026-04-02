15f2b7d8327270769b872015c975587e3aeaa2c4
dark1zinn <edilsonjuininho154@gmail.com>
2026-04-01 23:24:05 -0300

    bckup configs at home

--- modules/users/dark1zin/home.nix
+++ modules/users/dark1zin/home.nix
@@ -7,6 +7,26 @@
       home.username = config.preferences.user.name;
       home.homeDirectory = "/home/${config.preferences.user.name}";
       home.stateVersion = "25.11";
+      
+      # Backup existing config files before home-manager takes over
+      home.activation.backupExistingConfigs = inputs.home-manager.lib.hm.dag.entryBefore ["writeBoundary"] ''
+        backup_dir="$HOME/.config-backup-$(date +%Y%m%d-%H%M%S)"
+        files_to_backup=(
+          "$HOME/.Xresources"
+          "$HOME/.local/share/icons/default/index.theme"
+          "$HOME/.config/gtk-4.0/settings.ini"
+          "$HOME/.config/gtk-3.0/settings.ini"
+          "$HOME/.gtkrc-2.0"
+        )
+        
+        for file in "''${files_to_backup[@]}"; do
+          if [[ -f "$file" && ! -L "$file" ]]; then
+            $DRY_RUN_CMD mkdir -p "$backup_dir/$(dirname "''${file#$HOME/}")"
+            $DRY_RUN_CMD mv -v "$file" "$backup_dir/''${file#$HOME/}"
+            echo "Backed up $file to $backup_dir"
+          fi
+        done
+      '';
 
       home.sessionVariables = {
         XCURSOR_THEME = "macOS";
@@ -42,9 +62,43 @@
         extraPortals = [
           pkgs.xdg-desktop-portal-gtk
           pkgs.xdg-desktop-portal-gnome
-        ]; 
+        ];
+        config.common.default = "*";
       };
-      gtk.enable = true;
+      
+      # GTK configuration
+      gtk = {
+        enable = true;
+        
+        theme = {
+          name = "Tokyonight";
+          package = pkgs.tokyonight-gtk-theme.override {
+            colorVariants = [ "dark" ];
+            sizeVariants = [ "standard" ];
+            themeVariants = [ "default" ];
+            tweakVariants = [ ];
+          };
+        };
+        
+        iconTheme = {
+          name = "macOS";
+          package = pkgs.apple-cursor;
+        };
+        
+        gtk3.extraConfig = {
+          gtk-application-prefer-dark-theme = true;
+        };
+        
+        gtk4.extraConfig = {
+          gtk-application-prefer-dark-theme = true;
+        };
+      };
+      
+      # Configure Xresources
+      xresources.properties = {
+        "Xcursor.theme" = "macOS";
+        "Xcursor.size" = 26;
+      };
     };
   };
 }
