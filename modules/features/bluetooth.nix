1d9b6a59f02cec0ff9bfcaa20990121530c9f5d6
dark1zinn <dark1zinn00@gmail.com>
2026-06-21 20:55:48 -0300

    tweak bluetooth feature

--- modules/features/bluetooth.nix
+++ modules/features/bluetooth.nix
@@ -1,9 +1,36 @@
 {...}: {
-  flake.nixosModules.bluetooth = _: {
+  flake.nixosModules.bluetooth = {pkgs, ...}: {
     hardware = {
       enableAllFirmware = true;
-      bluetooth.enable = true;
-      bluetooth.powerOnBoot = false;
+      bluetooth = {
+        enable = true;
+        powerOnBoot = true;
+        settings = {
+          General = {
+            Experimental = true;
+            FastConnectable = true;
+          };
+          Policy = {
+            AutoEnable = true;
+          };
+        };
+      };
     };
+
+    boot.kernelModules = ["btusb"];
+
+    # IdeaPad laptops often leave hci0 soft-blocked at boot despite powerOnBoot.
+    systemd.services.bluetooth-rfkill-unblock = {
+      description = "Unblock Bluetooth rfkill after boot";
+      after = ["bluetooth.service" "systemd-rfkill.service"];
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = "${pkgs.util-linux}/bin/rfkill unblock bluetooth";
+      };
+    };
+
+    environment.systemPackages = with pkgs; [bluez];
   };
 }
