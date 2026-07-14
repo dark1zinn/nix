218be1c571e4983647dcf520c3d0a42aecd68068
dark1zinn <dark1zinn00@gmail.com>
2026-07-14 19:12:00 -0300

    fixed starship character

--- modules/users/dark1zin/programs/starship.nix
+++ modules/users/dark1zin/programs/starship.nix
@@ -1,19 +1,17 @@
 {...}: {
-  flake.nixosModules.dark1zin-starship = {pkgs, config, ...}: {
+  flake.nixosModules.dark1zin-starship = {pkgs, ...}: {
     environment.systemPackages = [pkgs.starship];
 
-    home-manager.users.${config.preferences.user.name} = {
-        programs.starship = {
-          enable = true;
-          presets = ["nerd-font-symbols"];
-          settings = {
-            character = {
-              format = "$symbol ";
-              success_symbol = "[\\$](bold purple)";
-              error_symbol = "[\\$](bold red)";
-            };
-          };
+    programs.starship = {
+      enable = true;
+      presets = ["nerd-font-symbols"];
+      settings = {
+        character = {
+          format = "$symbol ";
+          success_symbol = "[\\$](bold purple)";
+          error_symbol = "[\\$](bold red)";
         };
       };
+    };
   };
 }
